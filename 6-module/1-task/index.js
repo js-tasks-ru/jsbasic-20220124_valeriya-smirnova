16bf89ede0dddbfecd472a346d3590d992059ffe
Valeriya Smirnova <valesmirnova@ozon.ru>
2022-03-03 11:06:28 +0300

    Solve 6-1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,44 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.table = document.createElement("table");
+    this.table.innerHTML = `
+    <thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+   <thead>
+   `;
+    this.tbody = document.createElement("tbody");
+    this.table.appendChild(this.tbody);
+
+    for (const el of this.rows) {
+      this.tbody.insertAdjacentHTML(
+        "beforeEnd",
+        `<tr>
+      <td>${el.name}</td>
+      <td>${el.age}</td>
+       <td>${el.salary}</td>
+      <td>${el.city}</td>
+      <td><button>X</button></td>
+  </tr>`
+      );
+    }
+    this.table.addEventListener("click", this.onclick);
+  }
+  get elem() {
+    return this.table;
+  }
+  onclick(event) {
+    const btn = event.target.closest("button");
+    const trEl = event.target.closest("tr");
+    if (btn) {
+      trEl.remove();
+    }
   }
 }
